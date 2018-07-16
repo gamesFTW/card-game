fc36e2ccb301a9016d85b01c1b2b55ac775d3e92
alex-k <alex-k@yandex-team.ru>
2018-07-16 21:37:15 +0300

    Решил конфлиты после мерджа на сервере

--- src/infr/Event.ts
+++ src/infr/Event.ts
@@ -30,16 +30,17 @@ class Event<DataType = any, ExtraType = any> {
   }
 }
 
+let formatEventsForClient = (param:Array<Entity | Array<Entity>>): Array<Event> => {
+  let params = lodash.isArray(param) ? param : [param];
 
-class ClientEvent extends Event {
-  static convertFromEvent (event: Event, entity: Entity) : ClientEvent{
-    return new ClientEvent(event.type, event.data, entity);
-  }
+  let entities: Array<Entity> = lodash.flatten(param);
 
-  public constructor (type: string, data: any, entity: Entity) {
-    super(type, data, { id: entity.id });
-  }
+  let events =  entities.map((entity) => {
+    return entity.changes;
+  });
+
+  return lodash.flatten(events);
 }
 
 
-export {Event, ClientEvent};
+export {Event, formatEventsForClient};
