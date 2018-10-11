cf3b20a570e360aa9c62ad6cb2d1282a874ec28b
Alex Kalmakov <st00nsa@gmail.com>
2018-10-11 23:29:01 +0300

    config from env variables

--- game-server/src/config.ts
+++ game-server/src/config.ts
@@ -1,7 +1,9 @@
+const DOCKER_HOST = process.env['DOCKER_HOST_IP'] || '192.168.99.100';
+
 export default {
-  RABBIT_MQ_URL: 'amqp://192.168.99.100:5672',
-  REDIS_URL: 'redis://192.168.99.100:6379',
-  MONGO_URL: '192.168.99.100',
+  RABBIT_MQ_URL: `amqp://${DOCKER_HOST}:5672`,
+  REDIS_URL: `redis://${DOCKER_HOST}:6379`,
+  MONGO_URL: `${DOCKER_HOST}`,
   MONGO_PORT: 27017,
   DEV: true,
   MAIN_EXCHANGE: 'main'
