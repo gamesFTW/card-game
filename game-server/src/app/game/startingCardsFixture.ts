28d212b7492290169956d7f17c155ef8c1ca33ee
avorobyev <allah.taskbar@gmail.com>
2018-12-05 22:51:44 +0300

    Change starting fixtures

--- game-server/src/app/game/startingCardsFixture.ts
+++ game-server/src/app/game/startingCardsFixture.ts
@@ -1,19 +1,34 @@
 import * as lodash from 'lodash';
 
-let orc = {name: 'Орк', maxHp: 3, damage: 1, mannaCost: 2, movingPoints: 3};
-let orcHero = {name: 'Геройский орк', maxHp: 6, damage: 1, mannaCost: 0, movingPoints: 3};
+let hero = {name: 'Герой', maxHp: 6, damage: 1, mannaCost: 0, movingPoints: 3};
+
+let fat = {name: 'Толстокожая', maxHp: 6, damage: 3, mannaCost: 6, movingPoints: 3};
+let goblin = {name: 'Гоблин', maxHp: 4, damage: 2, mannaCost: 4, movingPoints: 3};
+let boar = {name: 'Кабан', maxHp: 4, damage: 2, mannaCost: 4, movingPoints: 3};
+let reptile = {name: 'Ящер', maxHp: 2, damage: 1, mannaCost: 2, movingPoints: 3};
+let skeleton = {name: 'Скелет', maxHp: 2, damage: 1, mannaCost: 2, movingPoints: 3};
+
+let units = [fat, goblin, boar, reptile, skeleton];
+
+function createUnits (): any {
+  return lodash.shuffle(
+    units.reduce((accumulator, card) => {
+      return accumulator.concat(lodash.range(5).map(i => card));
+    }, [])
+  );
+}
 
 let playerA = {
-  deck: lodash.range(20).map(i => orc),
+  deck: createUnits(),
   heroes: [
-    orcHero
+    hero
   ]
 };
 
 let playerB = {
-  deck: lodash.range(20).map(i => orc),
+  deck: createUnits(),
   heroes: [
-    orcHero
+    hero
   ]
 };
 
