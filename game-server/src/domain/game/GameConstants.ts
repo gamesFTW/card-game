404f6e5f2cc2bfa2991aac3a6acebc3e2cee405b
avorobyev <allah.taskbar@gmail.com>
2018-12-05 23:35:32 +0300

    Change starting heroes positions

--- game-server/src/domain/game/GameConstants.ts
+++ game-server/src/domain/game/GameConstants.ts
@@ -2,8 +2,8 @@ let GameConstants = {
   STARTING_HAND: 9,
   HANDICAP: 1,
   CARDS_PER_TURN: 3,
-  BOARD_WIDTH: 8,
-  BOARD_HEIGHT: 9
+  BOARD_WIDTH: 9,
+  BOARD_HEIGHT: 8
 };
 
 export {GameConstants};
