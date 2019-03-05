5ec3639d3309c3a4504d4f41201c87277454e27d
Alex Kalmakov <st00nsa@gmail.com>
2019-03-05 22:08:56 +0200

    Effects glow on title

--- unity-client/Assets/Scripts/Board/TileDisplay.cs
+++ unity-client/Assets/Scripts/Board/TileDisplay.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using UnibusEvent;
+using UnityEditor.Presets;
 
 public class TileDisplay : MonoBehaviour
 {
     public static readonly string TILE_MOUSE_LEFT_CLICK = "TILE_MOUSE_LEFT_CLICK";
     public static readonly string TILE_MOUSE_OVER = "TILE_MOUSE_OVER";
     public static readonly string TILE_MOUSE_EXIT = "TILE_MOUSE_EXIT";
+
+    //public SpriteGlow.SpriteGlowEffect glowEffectValues;
+    public Preset glowEffectValues;
+    //public SpriteGlow.SpriteGlowEffect glowEffect;
 
     public int x;
     public int y;
@@ -23,15 +28,34 @@ public class TileDisplay : MonoBehaviour
     void OnMouseDown()
     {
         Unibus.Dispatch<Point>(TILE_MOUSE_LEFT_CLICK, new Point(x, y));
+
     }
 
     void OnMouseOver()
     {
         Unibus.Dispatch<Point>(TILE_MOUSE_OVER, new Point(x, y));
+        highlightOn();
     }
 
     void OnMouseExit()
     {
         Unibus.Dispatch<Point>(TILE_MOUSE_EXIT, new Point(x, y));
+        highlightOff();
     }
+
+    private void highlightOn()
+    {
+        if (gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>() == null) {
+            SpriteGlow.SpriteGlowEffect effect = gameObject.AddComponent(typeof(SpriteGlow.SpriteGlowEffect)) as SpriteGlow.SpriteGlowEffect;
+            glowEffectValues.ApplyTo(effect);
+        }
+    }
+
+    private void highlightOff()
+    {
+        SpriteGlow.SpriteGlowEffect effect = gameObject.GetComponent<SpriteGlow.SpriteGlowEffect>();
+        Destroy(effect);
+
+    }
+
 }
