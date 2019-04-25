111e9bfa128fbfaa207393367f487282b046c555
Alex Kalmakov <st00nsa@gmail.com>
2019-04-26 00:24:03 +0300

    server side of socket io server without namespace support

--- game-server/src/infr/GodOfSockets.ts
+++ game-server/src/infr/GodOfSockets.ts
@@ -1,119 +1,89 @@
 import chalk from 'chalk';
+import * as koaSocketIO from 'socket.io';
+import * as koaIO from 'koa-socket-2';
+import * as Koa from 'koa';
 
-import * as net from 'net';
-import { Event } from './Event';
 import { EntityId } from './Entity';
 
-// const server = net.createServer((socket) => {
-//   socket.write('heloo\n', "utf-8");
-// }).on('error', (err) => {
-//   console.error(err);
-// });
-
-// // grab an arbitrary unused port.
-// server.listen({
-//     host: 'localhost',
-//     port: 3000,
-//   },
-//   () => {
-//     console.log('opened server on', server.address());
-//   }
-// );
-
 class GodOfSockets {
-  public userToSocket: Map<EntityId, net.Socket>;
-  private socketToUser: WeakMap<net.Socket, EntityId>;
-  private socketServer: net.Server;
+  public userToSocket: Map<EntityId, koaSocketIO.Socket>;
+  private socketToUser: WeakMap<koaSocketIO.Socket, EntityId>;
+  private gameIdToSockets: Map<EntityId, koaSocketIO.Socket[]>;
+  private koaSocketIO: koaIO;
 
   constructor () {
     this.userToSocket = new Map();
     this.socketToUser = new WeakMap();
-    this.socketServer = net.createServer();
-    // TODO добавить возможность поменять из env
-    this.socketServer.listen({
-      host: '0.0.0.0',
-      port: 3002
+    this.gameIdToSockets = new Map();
+  }
+
+  public registerNamespace = (namespace: String) => {
+    this.koaSocketIO.socket.of('/' + namespace);
+  }
+
+  public autoRegistrateUsers (koaSocketIO: koaIO): void {
+    this.koaSocketIO = koaSocketIO;
+
+    koaSocketIO.on('register', (ctx, data) => {
+      let playerId = data.playerId as EntityId;
+      let gameId = data.gameId as EntityId;
+      if (!playerId || !gameId) {
+        console.error(chalk.red('playerId & gameId must be set'));
+        return;
+        // throw new Error('PlayerId must be set');
+      }
+
+      this.socketToUser.set(ctx.socket, playerId);
+      this.userToSocket.set(playerId, ctx.socket);
+
+      if (!this.gameIdToSockets.get(gameId)) {
+        this.gameIdToSockets.set(gameId, []);
+      }
+      this.gameIdToSockets.get(gameId).push(ctx.socket);
+
+      console.info(chalk.yellow(`player ${playerId} is registred`));
     });
 
-    this.socketServer.addListener('connection', (socket: net.Socket) => {
-      console.log('player connected');
-      this.addPlayerSocket('1', socket);
-      // socket.write(JSON.stringify({type: "Hello"}), 'utf-8');
-
-      socket.on('error', (err: Error) => {
-        console.error(err);
-      });
-
-      socket.on('close', (hadError: boolean) => {
-        console.log('player disconnected');
-        this.removeSocket(socket);
-      });
-
-      socket.on('data', (dataBuffer: Buffer) => {
-        const data = dataBuffer.toString('utf-8');
-        //this.addPlayerSocket(playerId, socket);
-
-        console.log('<<', data);
+    koaSocketIO.on('connection', (ctx, data) => {
+      console.info(chalk.yellow(`Sombody connected ${data}`));
+      ctx.socket.on('beep', function (): void {
+        console.info(chalk.yellow('beep'));
+        ctx.socket.emit('boop');
       });
     });
-  }
 
-  public sendActions (gameId: EntityId, clientEvents: Array<any>): void {
-    console.log('Send Data to players');
-
-    let sendingData = {actions: clientEvents};
-    const data = JSON.stringify(sendingData);
-    this.userToSocket.forEach((socket, playerId) => {
-      socket.write(data, 'utf-8');
-      console.info(chalk.green(`${playerId} << ${data}`));
-    });
-  }
-
-  public sendEventsInGame (gameId: EntityId, PlayerId: EntityId, clientEvents: Array<any>): void {
-    //console.log
-  }
-
-  private addPlayerSocket (playerId: EntityId, socket: net.Socket): void {
-    this.userToSocket.set(playerId, socket);
-    console.info(chalk.yellow(`player ${playerId} is registred`));
-  }
-
-  private removeSocket (socket: net.Socket): void {
-    this.userToSocket.forEach((currentSocket, playerId) => {
-      if (socket === currentSocket) {
+    koaSocketIO.on('disconnect', (ctx, data) => {
+      let playerId = this.socketToUser.get(ctx.socket);
+      if (!playerId) {
+        console.info(chalk.yellow('Socket without registration is disconnected'));
+      } else {
         this.userToSocket.delete(playerId);
+        // TODO delete from this.gameIdToSockets.get(gameId)
         console.info(chalk.yellow(`User ${playerId} disconnected`));
       }
     });
   }
 
-  // public autoRegistrateUsers (koaSocketIO: koaIO): void {
+  public sendActions (gameId: EntityId, clientEvents: Array<any>): void {
+    // namespace now not supported
+    // this.koaSocketIO.socket.of('/' + gameId).emit('event', clientEvents);
 
-  //   koaSocketIO.on('register', (ctx, data) => {
-  //     let playerId = data.playerId as EntityId;
-  //     let gameId = data.playerId as EntityId;
-  //     if (!playerId || !gameId) {
-  //       console.error(chalk.red('playerId|gameId must be set'));
-  //       // throw new Error('PlayerId must be set');
-  //     } else {
-  //       this.socketToUser.set(ctx.socket, playerId);
-  //       this.userToSocket.set(playerId, ctx.socket);
-
-  //       console.info(chalk.yellow(`player ${playerId} is registred`));
-  //     }
-  //   });
-
-  //   koaSocketIO.on('disconnect', (ctx, data) => {
-  //     let playerId = this.socketToUser.get(ctx.socket);
-  //     if (!playerId) {
-  //       console.info(chalk.yellow('Socket without registration is disconnected'));
-  //     } else {
-  //       this.userToSocket.delete(playerId);
-  //       console.info(chalk.yellow(`User ${playerId} disconnected`));
-  //     }
-  //   });
-  // }
+    if (this.gameIdToSockets.get(gameId)) {
+      this.gameIdToSockets.get(gameId).forEach(socket => {
+        const playerId = this.socketToUser.get(socket);
+        console.info(chalk.yellow(`${playerId} <<< ${JSON.stringify(clientEvents)}`));
+        // socket.emit('event', clientEvents);
+        socket.emit('event');
+      });
+    }
+  }
+}
+interface KoaWithSocketIo extends Koa {
+  _io: SocketIO.Server;
 }
 
+// Register new namespace for game
+// TODO REMOVE NAMESPACES CUZ OF UNITY LIB DOES NOT SUPPORT IT
+
 const godOfSockets = new GodOfSockets();
-export { godOfSockets };
+export { godOfSockets, KoaWithSocketIo };
