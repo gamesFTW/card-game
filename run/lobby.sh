481657df1ddb8d61d9d3d41aed023840c7fb638b
Alex Kalmakov <st00nsa@gmail.com>
2019-04-30 23:22:09 +0300

    run sripts

---
+++ run/lobby.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export MONGO_IP=127.0.0.1  # set your docker agent ip address (not necessary for Win user)
+export DOCKER_HOST_IP=127.0.0.1
+cd ${DIR}/../lobby-server
+npm i
+npm run dev 
