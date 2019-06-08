24e9ad67fc7226d29b4b2c240ee428c5d279f5c9
Alex Kalmakov <st00nsa@gmail.com>
2019-06-09 00:09:45 +0300

    Remove client on react

--- client/src/UI/store/cards/index.ts
+++
@@ -1,6 +0,0 @@
-// public API
-import * as cardsActions from './actions';
-import * as countersSelectors from './selectors';
-import { reducer, CardsState } from './reducer';
-
-export { reducer, CardsState, cardsActions, countersSelectors };
--- client/src/UI/store/cards/selectors.ts
+++
@@ -1,4 +0,0 @@
-import { CardsState } from './index';
-
-export const getReduxCounter =
-  (state: CardsState) => state.reduxCounter;
