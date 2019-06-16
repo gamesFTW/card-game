5ae3b7ffa16f04f460100443632331424f076a33
avorobyev <allah.taskbar@gmail.com>
2019-06-16 10:24:11 +0300

    Add path highlight to no selection state

--- unity-client/Assets/Scenes/Game/Handlers/PlayerActionsOnBoard.cs
+++ unity-client/Assets/Scenes/Game/Handlers/PlayerActionsOnBoard.cs
@@ -201,11 +201,16 @@ public class NoSelectionsState
     public void Enable()
     {
         Unibus.Subscribe<UnitDisplay>(BoardCreator.UNIT_CLICKED_ON_BOARD, OnUnitSelectedOnBoard);
+        Unibus.Subscribe<UnitDisplay>(BoardCreator.UNIT_MOUSE_ENTER_ON_BOARD, OnUnitMouseEnterOnBoard);
+        Unibus.Subscribe<UnitDisplay>(BoardCreator.UNIT_MOUSE_EXIT_ON_BOARD, OnUnitMouseExitOnBoard);
     }
 
     private void Disable()
     {
+        this.playerActionsOnBoard.boardCreator.RemoveAllPathReach();
         Unibus.Unsubscribe<UnitDisplay>(BoardCreator.UNIT_CLICKED_ON_BOARD, OnUnitSelectedOnBoard);
+        Unibus.Unsubscribe<UnitDisplay>(BoardCreator.UNIT_MOUSE_ENTER_ON_BOARD, OnUnitMouseEnterOnBoard);
+        Unibus.Unsubscribe<UnitDisplay>(BoardCreator.UNIT_MOUSE_EXIT_ON_BOARD, OnUnitMouseExitOnBoard);
     }
 
     private void OnUnitSelectedOnBoard(UnitDisplay clickedUnitDisplay)
@@ -215,6 +220,19 @@ public class NoSelectionsState
             this.Disable();
             this.playerActionsOnBoard.ownUnitSelectedState.Enable(clickedUnitDisplay);
         }
+    }
+
+    private void OnUnitMouseEnterOnBoard(UnitDisplay clickedUnitDisplay)
+    {
+        if (clickedUnitDisplay.CardDisplay.IsAlly)
+        {
+            this.playerActionsOnBoard.boardCreator.ShowPathReach(clickedUnitDisplay);
+        }
+    }
+
+    private void OnUnitMouseExitOnBoard(UnitDisplay clickedUnitDisplay)
+    {
+        this.playerActionsOnBoard.boardCreator.RemoveAllPathReach();
     }
 }
 
