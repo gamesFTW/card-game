f8d13abced0141242016a0f5574eb9c00977c5b3
avorobyev <allah.taskbar@gmail.com>
2019-06-29 16:46:54 +0300

    Change range attack algorithm

--- game-server/src/domain/attackService/Bresenham.ts
+++ game-server/src/domain/attackService/Bresenham.ts
@@ -7,25 +7,33 @@ class Bresenham {
     let x2 = point2.x;
     let y2 = point2.y;
 
-    let dx = Math.abs(x2 - x1);
-    let dy = Math.abs(y2 - y1);
-    let sx = (x1 < x2) ? 1 : -1;
-    let sy = (y1 < y2) ? 1 : -1;
-    let err = dx - dy;
+    let xDist = Math.abs(x2 - x1);
+    let yDist = -Math.abs(y2 - y1);
+    let xStep = (x1 < x2 ? +1 : -1);
+    let yStep = (y1 < y2 ? +1 : -1);
+    let error = xDist + yDist;
 
     let dots = [];
+    dots.push(new Point(x1, y1));
 
-    while (true) {
-      dots.push(new Point(x1, y1));
+    while (x1 !== x2 || y1 !== y2) {
+      if (2 * error - yDist === xDist - 2 * error) {
+        error += yDist;
+        x1 += xStep;
+        dots.push(new Point(x1, y1));
 
-      if ((x1 === x2) && (y1 === y2)) break;
-      let e2 = 2 * err;
-      if (e2 > -dy && e2 < dx) {
-        dots.push(new Point(x1 + sx, y1));
-        dots.push(new Point(x1, y1 + sy));
+        error += xDist;
+        y1 += yStep;
+        dots.push(new Point(x1, y1));
+      } else if (2 * error - yDist > xDist - 2 * error) {
+        error += yDist;
+        x1 += xStep;
+        dots.push(new Point(x1, y1));
+      } else {
+        error += xDist;
+        y1 += yStep;
+        dots.push(new Point(x1, y1));
       }
-      if (e2 > -dy) { err -= dy; x1 += sx; }
-      if (e2 < dx) { err += dx; y1 += sy; }
     }
 
     return dots;
