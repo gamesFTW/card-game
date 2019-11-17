8c2ef584b775657e8b3843781e5d69195c4b5df7
Alex Kalmakov <st00nsa@gmail.com>
2019-11-18 00:23:47 +0200

    save working for 1 player

---
+++ ai/helpers.js
@@ -0,0 +1,30 @@
+import { turn } from "./turn.js";
+
+export const getAIDeckId = (game) => {
+    return game[`deckId${getAIPlayerId(game)}`];
+};
+  
+export const getAIPlayerId = (game) => {
+    if (isAIName(game.deckName1)) {
+      return "1"
+    }
+    if (isAIName(game.deckName2)) {
+      return "2"
+    }
+  
+    throw new Error(`there is now AI in: ${game}`);
+};
+
+export const isAIName = (deckName) => {
+    return deckName.startsWith("AI");
+}
+
+export const subToTurnOnEndOfTurn = async (socket, gameId, gameLobbyData) => {
+    socket.on("event", async ({actions}) => {
+        const isEndOfTurn = actions.some(({type}) => type === "EndTurnAction");
+        if (isEndOfTurn) {
+          await turn(gameId, gameLobbyData);
+        }
+    });
+}
+  
