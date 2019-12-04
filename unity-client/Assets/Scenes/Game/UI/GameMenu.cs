0d44ed57001ede797d27c973e0411a0ab53a9878
Andrey Vorobyev <allah.taskbar@gmail.com>
2019-12-04 14:29:02 +0300

    Add menu on escape button

---
+++ unity-client/Assets/Scenes/Game/UI/GameMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    private bool menuVisible = false;
+
+    private void Start()
+    {
+        this.transform.Find("Container/MainMenu").GetComponent<Button>().onClick.AddListener(this.OnMainMenuClick);
+        this.transform.Find("Container/Resume").GetComponent<Button>().onClick.AddListener(this.OnResumeClick);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (this.menuVisible)
+            {
+                this.HideMenu();
+            } else
+            {
+                this.ShowMenu();
+            }
+        }
+    }
+
+    private void ShowMenu()
+    {
+        this.transform.Find("Container").gameObject.SetActive(true);
+        this.menuVisible = true;
+    }
+
+    private void HideMenu()
+    {
+        this.transform.Find("Container").gameObject.SetActive(false);
+        this.menuVisible = false;
+    }
+
+    private void OnMainMenuClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void OnResumeClick()
+    {
+        this.HideMenu();
+    }
+}
